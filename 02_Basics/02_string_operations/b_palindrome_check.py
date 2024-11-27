aaacfb25da2894fe536baa7fadffe73ab0a8d466
saiharshithpalepu <87544222+saiharshithpalepu@users.noreply.github.com>
2024-11-27 02:27:42 +0000

    added new class06 files

---
+++ 02_Basics/02_string_operations/b_palindrome_check.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+Purpose: Demonstration of Palindrome check
+
+    palindrome strings
+        dad
+        mom
+
+Algorithms:
+-----------
+Step 1: Take the string in run-time and store in a variable
+Step 2: create a reverse string , from the given string, using string reversal
+Step 3: compare both with a condition and decide
+
+"""
+
+test_string = input("Enter any string:")
+print("test_string    :", test_string)
+
+
+# reverse string
+reverse_string = test_string[::-1]
+print("reverse_string :", reverse_string)
+
+print(test_string == reverse_string)
+
+print("test_string == reverse_string:", test_string == reverse_string)
+
+
+if test_string == reverse_string:
+    print(test_string, "is palindrome")
+else:
+    print(test_string, "is NOT a palindrome")    
+
+
+
+# Assignment: test if a given sentence is palindrome or not
+# HINT: ignore the whitespace and check
+
+def checkPalindrome(word):
+
+    word =  word.replace(" ","")
+    if word==word[::-1]:
+        print("the given word is palindrome")
+    else:
+        print("the given word is not palindrome")
+
+checkPalindrome("a santa at nasa")
